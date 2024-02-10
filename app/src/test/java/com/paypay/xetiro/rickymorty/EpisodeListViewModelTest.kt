ff5c7290d82090eedd6c39eb534d04a8ace68242
xetiro <rubengeraldes@gmail.com>
2024-02-10 13:10:16 +0900

    Refactored Unit test class. Updated dependencies.

---
+++ app/src/test/java/com/paypay/xetiro/rickymorty/EpisodeListViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.paypay.xetiro.rickymorty
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class EpisodeListViewModelTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
