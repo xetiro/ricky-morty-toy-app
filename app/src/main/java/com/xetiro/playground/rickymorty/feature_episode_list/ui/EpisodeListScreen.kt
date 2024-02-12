f1a7d6e1c6bf713371d1d5b28bede6ce4abf8a41
xetiro <rubengeraldes@gmail.com>
2024-02-12 16:59:52 +0900

    Updated the ui. Created Episode list with Episode cards.

--- app/src/main/java/com/xetiro/playground/rickymorty/feature_episode_list/ui/EpisodeListScreen.kt
+++ app/src/main/java/com/xetiro/playground/rickymorty/feature_episode_list/ui/EpisodeListScreen.kt
@@ -1,21 +1,71 @@
 package com.xetiro.playground.rickymorty.feature_episode_list.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.xetiro.playground.rickymorty.feature_episode_list.data.model.Episode
 
 
 /**
- * Description...
+ * UI of Episode List Screen
  *
  * Created by xetiro (aka Ruben Geraldes) on 2024/02/12.
  */
 @Composable
 fun EpisodeListScreen(viewModel: EpisodeListViewModel) {
     val uiState = viewModel.uiState.observeAsState().value!!
-    if(uiState.isLoading) {
-        Text(text = "LOADING...")
-    } else {
-        Text(text = "Fetched ${uiState.episodeList.size} Episodes")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if(uiState.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.padding(128.dp)
+            )
+        } else {
+            EpisodeListView(uiState.episodeList)
+        }
+    }
+}
+
+@Composable
+fun EpisodeListView(episodeList: List<Episode>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(episodeList.size) {
+            EpisodeCardView(episodeList[it])
+        }
+    }
+}
+
+@Composable
+fun EpisodeCardView(episode: Episode) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(
+            text = episode.name,
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            text = episode.episode,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
